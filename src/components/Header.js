3326428b3ea150b47e80e73e0384b9ecc2546043
ary27x <ary27x@gmail.com>
2023-10-03 02:11:24 +0530

    added files

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "../main.css"
+function Header()
+{
+return (
+    <div className = "header">
+        <p className = "heading"> Sorting Algorithm Visualizer</p>
+       <a href = "https://github.com/ary27x" target="_blank"><p className="link">ary27x</p></a>
+       
+    </div>
+)
+}
+export default Header
